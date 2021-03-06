2b39670dbca6c731961cb72a64e3286dcb1492d8
peepeetee <yuyangzhao17@gmail.com>
2021-03-06 01:31:46 -0600

    added new line to files that did not have new lines at end of file

--- keyboards/bm68rgb/rules.mk
+++ keyboards/bm68rgb/rules.mk
@@ -23,3 +23,7 @@ AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 #LTO_ENABLE = yes
+<<<<<<< HEAD
+=======
+
+>>>>>>> 88c039459... added new line to files that did not have new lines at end of file
