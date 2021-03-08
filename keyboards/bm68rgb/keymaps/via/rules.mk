d710f5444de194b7c55b6be0832b62b4bde9e52d
peepeetee <yuyangzhao17@gmail.com>
2021-03-07 23:50:38 -0600

    interim commit

--- keyboards/bm68rgb/keymaps/via/rules.mk
+++ keyboards/bm68rgb/keymaps/via/rules.mk
@@ -17,7 +17,7 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes
@@ -25,3 +25,4 @@ RGB_MATRIX_DRIVER = WS2812
 #LTO_ENABLE = yes
 
 LAYOUTS = 65_ansi
+VIA_ENABLE = yes
